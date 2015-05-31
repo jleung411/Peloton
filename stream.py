a771bb06f7e5a8fac311afb675a73d89df306742
jleung <jleung@carbonite>
2015-05-30 21:45:56 -0400

    some improvements

--- stream.py
+++ stream.py
@@ -3,11 +3,20 @@ import json
 import sys
 import re
 import traceback
-
-from collections import deque
+import bisect
+import logging
 
 from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+
+sh  = logging.StreamHandler(sys.stdout)
+sh.setLevel(logging.DEBUG)
+sh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+log = logging.getLogger("jleung.peloton.stream")
+log.setLevel(logging.DEBUG)
+log.addHandler(sh)
 
 
 class Stream:
@@ -15,52 +24,72 @@ class Stream:
     SERVER_ENDPOINT_URI = "https://api.pelotoncycle.com/quiz/next/"
 
     def __init__(self, stream_name):
-        self.stream_name = stream_name;
+        self.stream_name = stream_name
         self.stream_uri = self.SERVER_ENDPOINT_URI + self.stream_name
 
     def next_element(self):
         # return -1 if anything goes wrong
         data = {"last": -1, "current": -1, "stream": ""}
+
         try:
             response = requests.get(self.stream_uri)
             data = response.json()
         except:
             traceback.print_exc()
+
         return data
 
 
 class MergedStream:
 
-    stream1_current_element = 0;
-    stream2_current_element = 0;
-    merged_stream = deque()
-
     def __init__(self, stream1, stream2):
-        self.stream1 = stream1;
-        self.stream2 = stream2;
+        self.merged_stream = []
+
+        self.stream1 = stream1
+        self.stream2 = stream2
+
+        self.stream1_current_element = self.stream1.next_element()
+        self.stream2_current_element = self.stream2.next_element()
+
+        if self.stream1_current_element["current"] < self.stream2_current_element["current"]:
+            self.last_element = self.stream1_current_element["last"]
+            self.merged_stream.append(self.stream1_current_element["current"])
+            self.merged_stream.append(self.stream2_current_element["current"])
+        else:
+            self.last_element = self.stream2_current_element["last"]
+            self.merged_stream.append(self.stream2_current_element["current"])
+            self.merged_stream.append(self.stream1_current_element["current"])
+
+        log.debug("MergedStream::__init__()")
+        log.debug("stream1: {0}, stream2: {1}".format(stream1.stream_name, stream2.stream_name))
+        log.debug("stream1_current_element: {0}, stream2_current_element: {1}".format(self.stream1_current_element["current"], self.stream2_current_element["current"]))
+        log.debug("merged_stream: {0}".format(self.merged_stream))
+
 
     def next_element(self):
         # return -1 if anything goes wrong
         next_element = {"last": -1, "current": -1}
+
+        log.debug("MergedStream::next_element()")
+        log.debug("stream1_current_element: {0}, stream2_current_element: {1}".format(self.stream1_current_element["current"], self.stream2_current_element["current"]))
+
         try:
-            if self.stream1_current_element < self.stream2_current_element:
-                element = self.stream1.next_element()
-                self.stream1_current_element = element["current"]
+            if self.stream1_current_element["current"] < self.stream2_current_element["current"]:
+                element = self.stream1_current_element = self.stream1.next_element()
             else:
-                element = self.stream2.next_element()
-                self.stream2_current_element = element["current"]
+                element = self.stream2_current_element = self.stream2.next_element()
 
-            print "element: {0}".format(element)
-            print "stream1_current_element: {0}, stream2_current_element: {0}".format(self.stream1_current_element, self.stream2_current_element)
-            print "merged_stream: {0}".format(self.merged_stream)
-            print ""
+            log.debug("element: {0}".format(element))
+ 
+            bisect.insort(self.merged_stream, element["current"])
+            next_element["last"] = self.last_element
+            next_element["current"] = self.merged_stream.pop(0)
 
-            self.merged_stream.append(element)
-
-            next_element = self.merged_stream.popleft() 
-  
+            self.last_element = next_element["current"]
         except:
             traceback.print_exc()
+
+        log.debug("merged_stream: {0}".format(self.merged_stream))
 
         return next_element
 
@@ -71,6 +100,7 @@ class MergedStreamManager:
     def __init__(self):
         self.merged_stream = None
 
+    # This merged_stream singleton is not thread safe
     def get_instance(self, stream1, stream2):
         if self.merged_stream is None:
             self.merged_stream = MergedStream(stream1, stream2)
@@ -98,7 +128,7 @@ def merge():
             stream2 = Stream(stream_name2)
 
             merged_stream = merged_stream_manager.get_instance(stream1, stream2)
-#            response = merged_stream.next_element()
+            response = merged_stream.next_element()
         else:
             raise Exception("Invalid stream name supplied")
     except:
@@ -115,3 +145,5 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
+
+
--- stream_tests.py
+++ stream_tests.py
@@ -1,25 +1,36 @@
 import os
+import sys
+import json
 import unittest
 import stream
+import random
+import logging
 from stream import MergedStream, MergedStreamManager, Stream, valid_stream_name
 from mock import Mock
 
+sh  = logging.StreamHandler(sys.stdout)
+sh.setLevel(logging.DEBUG)
+sh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+log = logging.getLogger("jleung.peloton.stream_tests")
+log.setLevel(logging.DEBUG)
+log.addHandler(sh)
 
 class FlaskrTestCase(unittest.TestCase):
-
 
     def setUp(self):
         stream.app.config['TESTING'] = True
         self.app = stream.app.test_client()
-        self.stream1 = [2, 6, 11, 14, 15, 21]
-        self.stream2 = [10, 11, 22, 50, 88, 92]
+        stream.merged_stream_manager = MergedStreamManager()
+#        self.test_stream1 = [2, 6, 11, 14, 15, 21]
+#        self.test_stream2 = [10, 11, 22, 50, 88, 92]
 
  
-#    def tearDown(self):
-
+    def tearDown(self):
+        stream.merged_stream_manager = None
 
     def test_stream(self):
-        stream1 = Stream("stream1")
+        stream1 = Stream("stream{0}".format(random.randint(1, 10000)))
         assert stream1.stream_name == "stream1"
         assert stream1.stream_uri == "https://api.pelotoncycle.com/quiz/next/stream1"
 
@@ -35,35 +46,37 @@ class FlaskrTestCase(unittest.TestCase):
 
 
     def test_merged_stream(self):
-        stream1 = Stream("stream1")
-        stream1.next_element = Mock(return_value={"last": 2, "current": 6})
-
-        stream2 = Stream("stream2")
-        stream2.next_element = Mock(return_value={"last": 10, "current": 11})
-
+        stream1 = Stream("stream{0}".format(random.randint(1, 10000)))
+        stream2 = Stream("stream{0}".format(random.randint(1, 10000)))
         merged_stream = MergedStream(stream1, stream2)
 
         # grab first element
         current_element = merged_stream.next_element()
         last_element = current_element["last"]
 
-        for i in range(0, 3): 
-#            print "last_element: {0}".format(last_element)
-            print "current_element: {0} ".format(current_element["current"])
-
+        for i in range(0, 9):
+            log.debug("current_element: {0}, last_element: {1}".format(current_element["current"], current_element["last"]))
             assert last_element <= current_element["current"]
 
             current_element = merged_stream.next_element()
             last_element = current_element["current"]
 
-        next_element = merged_stream.next_element()
-        print next_element
-
 
     def test_merge_route(self):
-        stream.merged_stream_manager = MergedStreamManager()
-        response = self.app.get('quiz/merge/?stream1=test1&stream2=test2')
-        assert response.status_code == 200
+        last_response = self.app.get('quiz/merge/?stream1=test1&stream2=test2')
+        last_response_data = json.loads(last_response.data)
+
+        assert last_response.status_code == 200
+
+        for i in range(0, 9):
+            current_response = self.app.get('quiz/merge/?stream1=test1&stream2=test2')
+            current_response_data = json.loads(current_response.data)
+
+            assert current_response.status_code == 200
+            assert last_response_data["current"] <= current_response_data["current"]
+
+            last_reponse_data = current_response_data
+
 
 if __name__ == '__main__':
     unittest.main()
